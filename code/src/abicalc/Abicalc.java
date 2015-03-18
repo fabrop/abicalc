cf204eb24ac83a0574dbcc4a00c92428ce5cb163
FabooGuy <fabian.roepke@gmail.com>
2015-03-18 11:09:41 +0100

    Layout

--- code/src/abicalc/Abicalc.java
+++ code/src/abicalc/Abicalc.java
@@ -12,6 +12,8 @@ import javax.swing.SwingConstants;
 import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.JSeparator;
+import javax.swing.border.LineBorder;
+import javax.swing.BoxLayout;
 
 public class Abicalc extends JFrame {
 
@@ -40,27 +42,52 @@ public class Abicalc extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 800, 600);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.LIGHT_GRAY);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JPanel panel_title = new JPanel();
-		panel_title.setBounds(5, 5, 774, 44);
+		panel_title.setBackground(Color.DARK_GRAY);
+		panel_title.setBounds(10, 10, 764, 44);
 		contentPane.add(panel_title);
+		panel_title.setLayout(null);
 		
 		JLabel lblAbicalc = new JLabel("abicalc");
+		lblAbicalc.setBounds(25, -3, 92, 47);
+		lblAbicalc.setForeground(Color.WHITE);
 		lblAbicalc.setHorizontalAlignment(SwingConstants.LEFT);
 		lblAbicalc.setFont(new Font("Tahoma", Font.PLAIN, 28));
 		panel_title.add(lblAbicalc);
 		
 		JButton btnEinstellungen = new JButton("Einstellungen");
+		btnEinstellungen.setBounds(504, 10, 120, 23);
 		panel_title.add(btnEinstellungen);
 		
 		JButton btnZuruecksetzen = new JButton("Zur\u00FCcksetzen");
+		btnZuruecksetzen.setBounds(634, 10, 120, 23);
 		panel_title.add(btnZuruecksetzen);
 		
 		JPanel panel_main = new JPanel();
-		panel_main.setBounds(5, 60, 774, 491);
+		panel_main.setBounds(10, 65, 764, 431);
 		contentPane.add(panel_main);
+		
+		JPanel panel_unten = new JPanel();
+		panel_unten.setBackground(Color.DARK_GRAY);
+		panel_unten.setBounds(10, 507, 764, 44);
+		contentPane.add(panel_unten);
+		panel_unten.setLayout(null);
+		
+		JLabel lblNotenschnitt = new JLabel("Notenschnitt:");
+		lblNotenschnitt.setFont(new Font("Tahoma", Font.PLAIN, 28));
+		lblNotenschnitt.setForeground(Color.WHITE);
+		lblNotenschnitt.setBounds(29, 11, 166, 22);
+		panel_unten.add(lblNotenschnitt);
+		
+		JLabel label_Punkte = new JLabel("00");
+		label_Punkte.setForeground(Color.WHITE);
+		label_Punkte.setFont(new Font("Tahoma", Font.PLAIN, 28));
+		label_Punkte.setBounds(198, 11, 47, 22);
+		panel_unten.add(label_Punkte);
 	}
 }
