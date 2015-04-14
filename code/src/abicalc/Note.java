efc26b88ba1ce831a960278fe65bd304272f55e5
futzjug <achmed.lachnet@gmx.net>
2015-04-14 20:07:27 +0200

    zweiter Konstruktor für Noten hinzugefügt
    Zweiter Konstruktor mit Ziel vorhandene Textdateien zu lesen und daraus
    Noten Objekte zu erzeugen

--- code/src/abicalc/Note.java
+++ code/src/abicalc/Note.java
@@ -1,15 +1,18 @@
 package abicalc;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Note {
 	
 	//UntersteDatenstruktur
-	double gewichtung;
+	
 	String name;
 	int punkte;
+	double gewichtung;
 	Note naechstes;
 	
 	public Note(double g, String n, int p){//Konstruktor
@@ -17,6 +20,27 @@ public class Note {
 		name = n;
 		punkte = p;
 		}
+	
+	public Note(String n){
+		File datei = new File(n + "txt");
+		Scanner reader;
+		try {
+			reader = new Scanner(datei);
+			name = reader.nextLine();
+			punkte = Integer.parseInt(reader.nextLine());
+			gewichtung = Double.parseDouble(reader.nextLine());
+			if(reader.hasNextLine()){
+				naechstes.name = reader.nextLine();
+			}
+			else{
+				naechstes = null;
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	}
 	
 	public void noteSpeichern(){
 		File datei = new File(name + ".txt");
@@ -33,7 +57,7 @@ public class Note {
 			writer.write(punkte);
 			writer.write("" + gewichtung);
 			writer.write(naechstes.name);
-			
+			writer.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
