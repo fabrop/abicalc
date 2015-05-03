b1d0a12b42331ad651c9baaa43703d272294901c
FabooGuy <fabian.roepke@gmail.com>
2015-05-03 14:41:19 +0200

    Löschen FachUI, Kommentare bei Fach

--- code/src/abicalc/Fach.java
+++ code/src/abicalc/Fach.java
@@ -9,7 +9,7 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Fach extends Component implements java.io.Serializable{//Datenstruktur aus Noten
+public class Fach extends Component implements java.io.Serializable{//Datenstruktur aus Noten, die ein UI-ELement ist
 	
 	/**
 	 * 
@@ -20,26 +20,27 @@ public class Fach extends Component implements java.io.Serializable{//Datenstruk
 	LinkedList<Note>  notenliste;
 	Fach naechstes;
 	
-	public Fach(String s){//Konstruktor
-		name = s;
-		notenliste = new LinkedList<Note>();
+	public Fach(String s){ //Konstruktor
 		
+		//Verkette Liste aus Noten und Name des Fachs mithilfe des übergebenen Strings
+		name = s;
+		notenliste = new LinkedList<Note>();		
+		
+		//Panel mit Label und Button und Layout einer Tabelle mit 1 Zeile
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.GRAY);
-		
-		
-
 		panel.setLayout(new GridLayout(1, 0, 0, 0));
 		
+		//Label mit Fächernamen
 		JLabel lbl = new JLabel(name);
 		panel.add(lbl);
 		
+		//Button zum Noten bearbeiten
 		JButton btn = new JButton("Noten bearbeiten...");
 		btn.setBounds(641, 10, 113, 23);
 		panel.add(btn);
 		
-		
-
+		//Stellt sicher, dass UI aktualisiert wird
 		Abicalc.panel_scrollContentHJ1.add(panel);
 		Abicalc.panel_FaecherHJ1.validate();
 		Abicalc.panel_FaecherHJ1.repaint();
