f1b8111761c0b44ef71fb193a52fd260004e99f5
futzjug <achmed.lachnet@gmx.net>
2015-05-05 19:47:41 +0200

    Strukturänderung Fach

--- code/src/abicalc/Fach.java
+++ code/src/abicalc/Fach.java
@@ -30,40 +30,42 @@ public class Fach extends Component implements java.io.Serializable{//Datenstruk
 		name = s;
 		notenliste = new LinkedList<Note>();		
 		
-		//Panel mit Label und Button und Layout einer Tabelle mit 1 Zeile
-		JPanel panel = new JPanel();
-		panel.setBackground(Color.LIGHT_GRAY);
-		panel.setLayout(new GridLayout(1, 0, 0, 0));
-		
-		
-		//Label mit Fächernamen
-		JLabel lbl = new JLabel("   "+name);
-		panel.add(lbl);
-		
-		//Button zum Noten bearbeiten
-		JButton btn = new JButton("Noten bearbeiten...");
-		btn.setBounds(641, 10, 113, 23);
-		panel.add(btn);
-		
-		btn.addActionListener(new java.awt.event.ActionListener() {		//Code fürs Fach hinzufügen
-	        public void actionPerformed(java.awt.event.ActionEvent e) {
-	          
-	        	openDialog();
-	        	
-	        	//Stellt sicher, dass UI aktualisiert wird
-	        	panel.validate();
-	    		panel.repaint();
-	        }
-	    });
-			
-		
-		//Panel wird hinzugefügt
-		jpnl.add(panel);
-		
+		fachPanelHinzufuegen(jpnl);
 		
 	}
 	
-	
+	public void fachPanelHinzufuegen(JPanel jpnl){
+		//Panel mit Label und Button und Layout einer Tabelle mit 1 Zeile
+				JPanel panel = new JPanel();
+				panel.setBackground(Color.LIGHT_GRAY);
+				panel.setLayout(new GridLayout(1, 0, 0, 0));
+				
+				
+				//Label mit Fächernamen
+				JLabel lbl = new JLabel("   "+name);
+				panel.add(lbl);
+				
+				//Button zum Noten bearbeiten
+				JButton btn = new JButton("Noten bearbeiten...");
+				btn.setBounds(641, 10, 113, 23);
+				panel.add(btn);
+				
+				btn.addActionListener(new java.awt.event.ActionListener() {		//Code fürs Fach hinzufügen
+			        public void actionPerformed(java.awt.event.ActionEvent e) {
+			          
+			        	openDialog();
+			        	
+			        	//Stellt sicher, dass UI aktualisiert wird
+			        	panel.validate();
+			    		panel.repaint();
+			        }
+			    });
+					
+				
+				//Panel wird hinzugefügt
+				jpnl.add(panel);
+				
+	}
 	
 	public void openDialog() {
 
