1ea81b0d523ab630f043896892dfd3c67d410c81
FabooGuy <fabian.roepke@gmail.com>
2015-05-06 02:16:35 +0200

    Sync

--- code/src/abicalc/Abicalc.java
+++ code/src/abicalc/Abicalc.java
@@ -87,7 +87,7 @@ public class Abicalc extends JFrame{
 		JTabbedPane tabbedPane_Halbjahre = new JTabbedPane(JTabbedPane.TOP);
 		panel_main.add(tabbedPane_Halbjahre);
 		
-		Halbjahr[] halbjahre = new Halbjahr[5];
+		//Halbjahr[] halbjahre = new Halbjahr[5];
 		halbjahre[0] = new Halbjahr("hj1", "11.1", tabbedPane_Halbjahre, panel_main);
 		halbjahre[1] = new Halbjahr("hj2", "11.2", tabbedPane_Halbjahre, panel_main);
 		halbjahre[2] = new Halbjahr("hj3", "12.1", tabbedPane_Halbjahre, panel_main);
